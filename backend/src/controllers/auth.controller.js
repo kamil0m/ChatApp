bb37d9b2c3d5fc33a6e03041c21e3f42d3dec23f
Kamil Markowski <kamil0markowski@gmail.com>
2025-05-16 00:09:23 +0200

    databse connected

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = (req, res) => {
+    const {fullName, email, password} = req.body;
+    try {
+        // hash password
+        
+    } catch (error) {
+        
+    }
+}
+export const login = (req, res) => {
+    res.send('Login route');
+}
+export const logout = (req, res) => {
+    res.send('Logout route');
+}
