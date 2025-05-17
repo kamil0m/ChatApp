406f72f9e7a98e5a6d18584feb4b00241839a732
Kamil Markowski <kamil0markowski@gmail.com>
2025-05-17 02:22:03 +0200

    authentification check added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // Middleware to parse JSON request body
+app.use(cookieParser()); // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes);
 
